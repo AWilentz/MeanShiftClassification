f73667090c7bac185b12c5ef8984f29222706e98
Joshua Price <pricejoshuam@gmail.com>
2023-12-10 21:28:00 -0500

    adding census tranform

--- classification.py
+++ classification.py
@@ -10,12 +10,36 @@ def load_image(image_path):
     return luv
 
 
+def census_transform(luv_img):
+    # Drawn from https://stackoverflow.com/questions/37203970/opencv-grayscale-mode-vs-gray-color-conversion
+    # Initialize output array
+    img_rgb = cv2.cvtColor(luv_img, cv2.COLOR_LUV2RGB)
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+
+    h, w = img_gray.shape
+    census = np.zeros((h - 2, w - 2), dtype='uint8')
+
+    # centre pixels, which are offset by (1, 1)
+    cp = img_gray[1:h - 1, 1:w - 1]
+
+    # offsets of non-central pixels
+    offsets = [(u, v) for v in range(3) for u in range(3) if not u == 1 == v]
+
+    # Do the pixel comparisons
+    for u, v in offsets:
+        x = (census << 1)
+        y = (img_gray[v:v + h - 2, u:u + w - 2] >= cp)
+        census = (census << 1) | (img_gray[v:v + h - 2, u:u + w - 2] >= cp)
+
+    return census
+
 def ms_classify(input_img, spatial=False):
     num_cols = input_img.shape[1]
     num_rows = input_img.shape[0]
     img = input_img.reshape((-1, 3))
 
-    spatial = True
+    #census = census_transform(input_img)
+
     if spatial is True:
         col_val = np.array([np.arange(num_cols)])
         col_mat = np.repeat(col_val, num_rows, axis=0)
@@ -28,7 +52,9 @@ def ms_classify(input_img, spatial=False):
         img = np.hstack((img, row_col))
         img = np.hstack((img, col_col))
 
-    bandwidth = 45
+    #img = np.hstack((img, census))
+
+    bandwidth = 18
     #bandwidth = estimate_bandwidth(img, quantile=0.2, n_samples=500)
     ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
     ms.fit(img)
@@ -64,10 +90,19 @@ def ms_classify(input_img, spatial=False):
     '''
 
     labeled_img = labels.reshape(input_img.shape[0:2])
+    # cv2.imshow('Census', census)
+    i = 1
     for labelnum in range(len(labels_unique)):
 
         labelsN = (labeled_img==labelnum).astype(np.uint8)
-        cv2.imshow('Label ' + str(labelnum), labelsN*255)
+        n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(labelsN, connectivity=4)
+        large_enough_objs = stats[stats[:, 4] > 150]
+        num_objs = large_enough_objs[large_enough_objs[:, 4] < 1000,:].shape[0]
+        cv2.imshow('Label: ' + str(labelnum) + ', Num objs: ' + str(num_objs), labelsN*255)
+
+        if num_objs > 0:
+            print("Class " + str(i) + ": " + str(num_objs) + str(" objects"))
+            i += 1
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
