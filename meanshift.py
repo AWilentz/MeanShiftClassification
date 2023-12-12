2ec0af9106e920aa227fc717b1b034b515b4a408
Alex Wilentz <awilentz@college.harvard.edu>
2023-12-12 11:11:16 -0500

    debugging

--- meanshift.py
+++ meanshift.py
@@ -12,7 +12,7 @@ def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
 
 def mean_shift(image, bandwidth=3):
-    data = image.reshape((-1, 3))
+    data = image.reshape((-1, len(image[0])))
     clusters = data.copy()
 
     for i in tqdm(range(len(clusters))):
@@ -23,26 +23,25 @@ def mean_shift(image, bandwidth=3):
 
             dists = np.sqrt(((x-data)**2).sum(axis=1))
             weights = (1 / (bandwidth*math.sqrt(2*math.pi))) * np.exp(-0.5*((dists / bandwidth)**2))
-            tiled_weights = np.tile(weights, [len(x), 1])
+            # weights = [1 if dist <= bandwidth else 0 for dist in dists]
+            tiled_weights = np.tile(weights, [len(x), 1]).transpose()
             
             weights_sum = sum(weights)
-            x_new = np.multiply(tiled_weights.transpose(), data).sum(axis=0) / weights_sum
+            x = np.multiply(tiled_weights, data).sum(axis=0) / max(weights_sum, 1)
 
-            if euclidean_distance(x_prev, x_new) < .1:
+            if euclidean_distance(x_prev, x) < .001:
                 break
-
-            x = x_new
 
         clusters[i] = x
 
     centers = []
     labels = []
 
+    num_clusters = 0
     for point in clusters:
-        num_clusters = 0
         clustered = False
         for idx, center in enumerate(centers):
-            if euclidean_distance(point, center) <= 10:
+            if euclidean_distance(point, center) <= 2:
                 labels.append(idx)
                 clustered = True
                 break
