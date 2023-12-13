7a12acdbda4968ea1a7a5b141e54adfb36fb417a
Alex Wilentz <awilentz@college.harvard.edu>
2023-12-13 02:49:42 -0500

    finished meanshift implementation

--- meanshift.py
+++ meanshift.py
@@ -1,38 +1,43 @@
-# 1. Initialize random seed and window W.
-# 2. Calculate the center of gravity (mean) of W.    
-# 3. Shift the search window to the mean.    
-# 4. Repeat Step 2 until convergence.
-
 import numpy as np
 import cv2
 from tqdm import tqdm
 import math
 
+
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
 
 def mean_shift(image, bandwidth=3, texture=False):
-    data = image.reshape((-1, len(image[0])))
+    data = image.reshape((-1, len(image[0]))).astype(float)
     clusters = data.copy()
 
-    for i in tqdm(range(len(clusters))):
-        x = clusters[i]
+    max_shift = 1
 
-        while True:
+    shifted = [False] * len(data)
+    while max_shift > .1:
+        max_shift = 0
+        for i in tqdm(range(0, len(clusters))):
+            
+            if shifted[i]:
+                continue
+
+            x = clusters[i]
             x_prev = x
 
             dists = np.sqrt(((x-data)**2).sum(axis=1))
             weights = (1 / (bandwidth*math.sqrt(2*math.pi))) * np.exp(-0.5*((dists / bandwidth)**2))
-            # weights = [1 if dist <= bandwidth else 0 for dist in dists]
             tiled_weights = np.tile(weights, [len(x), 1]).transpose()
             
-            weights_sum = sum(weights)
+            weights_sum = np.sum(weights)
             x = np.multiply(tiled_weights, data).sum(axis=0) / max(weights_sum, 1)
 
-            if euclidean_distance(x_prev, x) < .001:
-                break
+            dist = euclidean_distance(x_prev, x)
+            if dist > max_shift:
+                max_shift = dist
+            if dist < .1:
+                shifted[i] = True
 
-        clusters[i] = x
+            clusters[i] = x
 
     centers = []
     labels = []
@@ -41,7 +46,7 @@ def mean_shift(image, bandwidth=3, texture=False):
     for point in clusters:
         clustered = False
         for idx, center in enumerate(centers):
-            if euclidean_distance(point, center) <= 2:
+            if euclidean_distance(point, center) <= 1:
                 labels.append(idx)
                 clustered = True
                 break
