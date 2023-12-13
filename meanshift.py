334ff0efb809b51bd798231b2d2d598ca351bc38
Alex Wilentz <awilentz@college.harvard.edu>
2023-12-13 03:01:36 -0500

    differing kernel bandwidths

--- meanshift.py
+++ meanshift.py
@@ -1,4 +1,4 @@
-import numpy as np
+import s as np
 import cv2
 from tqdm import tqdm
 import math
@@ -7,7 +7,7 @@ import math
 def euclidean_distance(x1, x2):
     return np.sqrt(np.sum((x1 - x2) ** 2))
 
-def mean_shift(image, bandwidth=3, texture=False):
+def mean_shift(image, bandwidth=20, texture=False, bandwidth_texture=20):
     data = image.reshape((-1, len(image[0]))).astype(float)
     clusters = data.copy()
 
@@ -26,7 +26,12 @@ def mean_shift(image, bandwidth=3, texture=False):
 
             dists = np.sqrt(((x-data)**2).sum(axis=1))
             weights = (1 / (bandwidth*math.sqrt(2*math.pi))) * np.exp(-0.5*((dists / bandwidth)**2))
-            tiled_weights = np.tile(weights, [len(x), 1]).transpose()
+            if texture:
+                weights_texture = (1 / (bandwidth_texture*math.sqrt(2*math.pi))) * np.exp(-0.5*((dists / bandwidth_texture)**2))
+                tiled_weights = np.concatenate((np.tile(weights, [len(x), 1]).transpose(),
+                                                np.tile(weights_texture, [len(x), 1]).transpose()), axis=-1)
+            else:
+                tiled_weights = np.tile(weights, [len(x), 1]).transpose()
             
             weights_sum = np.sum(weights)
             x = np.multiply(tiled_weights, data).sum(axis=0) / max(weights_sum, 1)
